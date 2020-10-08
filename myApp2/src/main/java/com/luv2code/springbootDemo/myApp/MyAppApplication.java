60305354f437abe3708b828a5797684a146a1d1c
Youness TIZI <younesstizi@gmail.com>
2020-10-08 09:58:51 +0100

    Initialisation de projet

---
+++ myApp2/src/main/java/com/luv2code/springbootDemo/myApp/MyAppApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootDemo.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
